ae5dc1df96b844e9a8642def56fb36d56277e1b1
Cancey Dejean <canceyd@gmail.com>
2022-07-05 21:25:06 -0500

    Removed dead image path

--- components/subComponents/images.js
+++ components/subComponents/images.js
@@ -1,5 +1,4 @@
 import About from "../../public/images/about.jpeg"
 import BigLogoImg from "../../public/images/my-logo.png"
-import Screen1 from "../../public/images/dekker-store.jpg"
 
-export { About, BigLogoImg, Screen1 }
+export { About, BigLogoImg }
